d2964af1b97a23780b5d5905c891e16d58e38697
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-03 13:22:59 +0100

    SortedSet/TreeSet added to ProductCatalogue

--- src/main/java/set/ProductCatalogue.java
+++ src/main/java/set/ProductCatalogue.java
@@ -2,16 +2,32 @@ package set;
 
 import com.krisdrum.collections.Product;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class ProductCatalogue implements Iterable<Product> {
-    private final Set<Product> products = new TreeSet<>(Product.BY_NAME);
+    private final SortedSet<Product> products = new TreeSet<>(Product.BY_WEIGHT);
 
     public void isSupliedBy(Supplier supplier) {
         products.addAll(supplier.products());
+    }
+
+    public Set<Product> lightVanProducts() {
+        Product heaviestLightVanProduct = findHeaviestLightVanProduct();
+        return products.headSet(heaviestLightVanProduct);
+    }
+
+    public Set<Product> heavyVanProducts() {
+        Product heaviestLightVanProduct = findHeaviestLightVanProduct();
+        return products.tailSet(heaviestLightVanProduct);
+    }
+
+    public Product findHeaviestLightVanProduct() {
+        for (Product product: products) {
+            if (product.getWeight() > 20) {
+                return product;
+            }
+        }
+        return products.last();
     }
 
     @Override
--- src/test/java/com/krisdrum/collections/set/ProductCatalogueTest.java
+++ src/test/java/com/krisdrum/collections/set/ProductCatalogueTest.java
@@ -2,9 +2,9 @@ package com.krisdrum.collections.set;
 
 import org.junit.Test;
 import set.ProductCatalogue;
-
 import static com.krisdrum.collections.ProductFixtures.door;
 import static com.krisdrum.collections.ProductFixtures.floorPanel;
+import static com.krisdrum.collections.ProductFixtures.window;
 import static com.krisdrum.collections.SupplierFixture.bobs;
 import static com.krisdrum.collections.SupplierFixture.kates;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -18,6 +18,26 @@ public class ProductCatalogueTest {
         catalogue.isSupliedBy(bobs);
         catalogue.isSupliedBy(kates);
 
-        assertThat(catalogue, containsInAnyOrder(door, floorPanel));
+        assertThat(catalogue, containsInAnyOrder(door, floorPanel, window));
+    }
+
+    @Test
+    public void shouldFindLightVanProducts() throws Exception {
+        ProductCatalogue catalogue = new ProductCatalogue();
+
+        catalogue.isSupliedBy(bobs);
+        catalogue.isSupliedBy(kates);
+
+        assertThat(catalogue.lightVanProducts(), containsInAnyOrder(window));
+    }
+
+    @Test
+    public void shouldFindHeavyVanProducts() throws Exception {
+        ProductCatalogue catalogue = new ProductCatalogue();
+
+        catalogue.isSupliedBy(bobs);
+        catalogue.isSupliedBy(kates);
+
+        assertThat(catalogue.heavyVanProducts(), containsInAnyOrder(floorPanel, door));
     }
 }
