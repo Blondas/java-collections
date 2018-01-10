5e50ddf6fb6237ca89c64c174d1cc5178f2ccad5
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-10 11:09:21 +0100

    forEach

--- src/main/java/com/krisdrum/collections/map/Java8Enhancements.java
+++ src/main/java/com/krisdrum/collections/map/Java8Enhancements.java
@@ -16,7 +16,29 @@ public class Java8Enhancements {
         idToProduct.put(2, ProductFixtures.floorPanel);
         idToProduct.put(3, ProductFixtures.window);
 
+        // getOrDefault:
         Product result = idToProduct.getOrDefault(10, defaultProduct);
         System.out.println(result);
+
+        // replace:
+        Product result1 = idToProduct.replace(1, new Product("Big Door", 50));
+        System.out.println(result1);
+        System.out.println(idToProduct.get(1));
+
+        //  replaceAll:
+        idToProduct.replaceAll((id, oldProduct) ->
+            new Product(id, oldProduct.getName(), oldProduct.getWeight() + 10));
+
+        System.out.println(idToProduct);
+
+        // computeIfAbsent:
+        Product result2 = idToProduct.computeIfAbsent(10, (id) -> new Product(id, "CustomProduct", 10));
+        System.out.println(result2);
+
+        // forEach:
+
+        idToProduct.forEach((key, value) ->
+                System.out.println(key + " -> " + value)
+        );
     }
 }
