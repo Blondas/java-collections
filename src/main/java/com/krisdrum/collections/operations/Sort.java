ac6a2849fcd665301f2aecd59c8c82ef529eddb7
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-10 12:12:34 +0100

    Collections .shuffle() .rotate() .sort()

---
+++ src/main/java/com/krisdrum/collections/operations/Sort.java
@@ -0,0 +1,25 @@
+package com.krisdrum.collections.operations;
+
+import com.krisdrum.collections.Product;
+import com.krisdrum.collections.map.viewsOverMaps.ProductFixtures;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(ProductFixtures.door);
+        products.add(ProductFixtures.floorPanel);
+        products.add(ProductFixtures.window);
+
+        System.out.println(products);
+
+        Collections.sort(products, Product.BY_NAME);
+        System.out.println(products);
+
+        Collections.sort(products, Product.BY_WEIGHT);
+        System.out.println(products);
+    }
+}
